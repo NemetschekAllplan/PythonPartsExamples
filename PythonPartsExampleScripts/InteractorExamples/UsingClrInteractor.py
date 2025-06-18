5eb6e56bd03247adffb3cfc0f6ba7fbf38dbf074
Bartlomiej Marciniec <bmarciniec@allplan.com>
2025-06-18 20:18:46 +0200

    Fix the example UsingClrInteractor

--- PythonPartsExampleScripts/InteractorExamples/UsingClrInteractor.py
+++ PythonPartsExampleScripts/InteractorExamples/UsingClrInteractor.py
@@ -1,104 +1,90 @@
-"""
-Script for UsingClrInteractor
-"""
+""" Example script showing the integration of WPF technology (C#) into a PythonPart script"""
 
-import sys
-
-import NemAll_Python_Geometry as AllplanGeo
-import NemAll_Python_BaseElements as AllplanBaseElements
-import NemAll_Python_BasisElements as AllplanBasisElements
-import NemAll_Python_IFW_Input as AllplanIFW
-import NemAll_Python_AllplanSettings as AllplanSettings
-
-etc_path = AllplanSettings.AllplanPaths.GetPythonPartsEtcPath()
-
-
-#----------------- using pythonnet, see http://pythonnet.github.io/
+from pathlib import Path
 
 import clr
 
-clr.AddReference(etc_path + "\\PythonPartsExampleScripts\\InteractorExamples\\PythonWPFConnection\\Bin\\PythonWPFConnection.dll")
+import NemAll_Python_Geometry as AllplanGeo
+import NemAll_Python_BaseElements as AllplanBaseElements
+import NemAll_Python_IFW_Input as AllplanIFW
+import NemAll_Python_IFW_ElementAdapter as AllplanElementAdapter
 
-from PythonWPFConnection import BoxDialog
-from PythonWPFConnection import NameValueArgs
+from BuildingElement import BuildingElement
+from CreateElementResult import CreateElementResult
+from TypeCollections.ModelEleList import ModelEleList
+from BaseInteractor import BaseInteractor, BaseInteractorData
+
+
+# ----------------- using pythonnet, see http://pythonnet.github.io/
+
+script_path = Path(__file__).resolve()
+
+clr.AddReference(str(script_path.parent / "PythonWPFConnection/Bin/PythonWPFConnection.dll"))
+
+from PythonWPFConnection import BoxDialog, NameValueArgs
 
 print('Load UsingClrInteractor.py')
 
 
-def check_allplan_version(build_ele, version):
-    """
-    Check the current Allplan version
+def check_allplan_version(_build_ele: BuildingElement, _version: float) -> bool:
+    """Check the current Allplan version
 
     Args:
-        build_ele: the building element.
-        version:   the current Allplan version
+        _build_ele: the building element.
+        _version:   the current Allplan version
 
     Returns:
         True/False if version is supported by this script
     """
 
-    # Delete unused arguments
-    del build_ele
-    del version
-
     # Support all versions
     return True
 
 
-def create_element(build_ele, doc):
-    """
-    Creation of element (only necessary for the library preview)
+def create_element(_build_ele: BuildingElement, _doc: AllplanElementAdapter.DocumentAdapter) -> CreateElementResult:
+    """Creation of an element only for the library preview
 
     Args:
-        build_ele: the building element.
-        doc:       input document
+        _build_ele: the building element.
+        _doc:       input document
+
+    Returns:
+        CreateElementResult with the created element
     """
 
-    del build_ele
-    del doc
+    model_ele_list = ModelEleList()
+    model_ele_list.append_geometry_3d(AllplanGeo.Polyhedron3D.CreateCuboid(1000, 2000, 3000))
 
-    com_prop = AllplanBaseElements.CommonProperties()
-
-    com_prop.GetGlobalProperties()
-
-    model_ele_list = [AllplanBasisElements.ModelElement3D(com_prop, AllplanGeo.Polyhedron3D.CreateCuboid(1000, 2000, 3000))]
-
-    return (model_ele_list, None, None)
+    return CreateElementResult(model_ele_list)
 
 
-def create_interactor(coord_input, pyp_path, str_table_service):
-
-    """
-    Create the interactor
+def create_interactor(interactor_data: BaseInteractorData) -> BaseInteractor:
+    """ Create the interactor
 
     Args:
-        coord_input:        coordinate input
-        pyp_path:           path of the pyp file
-        str_table_service:  string table service
+        interactor_data:  the interactor data containing the coordinate input and other necessary information.
+
+    Returns:
+        The interactor instance
     """
 
-    return UsingClrInteractor(coord_input, pyp_path, str_table_service)
+    return UsingClrInteractor(interactor_data)
 
 
-class UsingClrInteractor():
-    """
-    Definition of class UsingClrInteractor
-    """
+class UsingClrInteractor(BaseInteractor):
+    """Definition of class UsingClrInteractor"""
 
-    def __init__(self, coord_input, pyp_path, str_table_service):
-        """
-        Initialization of class UsingClrInteractor
+    def __init__(self, interactor_data: BaseInteractorData):
+        """Initialize
 
         Args:
-            coord_input:        coordinate input
-            pyp_path:           path of the pyp file
-            str_table_service:  string table service
+            interactor_data:  the interactor data containing the coordinate input and other necessary information.
         """
 
-        self.coord_input       = coord_input
-        self.pyp_path          = pyp_path
-        self.str_table_service = str_table_service
+        self.coord_input    = interactor_data.coord_input
+        self.model_ele_list = ModelEleList()
 
+        # Show the WPF dialog
         self.box_length = 1000.
         self.box_width  = 2000.
         self.box_height = 3000.
@@ -108,16 +94,13 @@ class UsingClrInteractor():
         self.box_dialog.ShowInTaskbar = False
         self.box_dialog.Show()
 
+        # subscribe to events
         self.box_dialog.UpdateValue += self.on_value_changed_handler
-
-        self.com_prop = AllplanBaseElements.CommonProperties()
 
         self.coord_input.InitFirstPointInput(AllplanIFW.InputStringConvert("From point"))
 
-
     def modify_element_property(self, page, name, value):
-        """
-        Modify property of element
+        """Modify property of element
 
         Args:
             page:   the page of the property
@@ -125,10 +108,8 @@ class UsingClrInteractor():
             value:  new value for property.
         """
 
-
     def on_cancel_function(self):
-        """
-        Check for input function cancel in case of ESC
+        """Check for input function cancel in case of ESC
 
         Returns:
             True/False for success.
@@ -138,24 +119,42 @@ class UsingClrInteractor():
 
         return True
 
-
     def on_preview_draw(self):
-        """
-        Handles the preview draw event
-        """
+        """Handles the preview draw event"""
 
         self.draw_preview(self.coord_input.GetCurrentPoint().GetPoint())
 
+    def on_control_event(self, event_id: int) -> bool:
+        """Handles the control event
+
+        Args:
+            event_id:  the event id.
+
+        Returns:
+            True/False for success.
+        """
+        return False
+
+    def on_value_input_control_enter(self) -> bool:
+        """Handles the value input control enter event
+
+        Returns:
+            True/False for success.
+        """
+        return False
 
     def on_mouse_leave(self):
-        """
-        Handles the mouse leave event
-        """
+        """Handles the mouse leave event"""
 
         self.on_preview_draw()
 
+    def on_value_changed_handler(self, _sender: BoxDialog, args: NameValueArgs) -> None:
+        """ Handles the value changed event from the box dialog
 
-    def on_value_changed_handler(self, sender, args):
+        Args:
+            _sender: the sender of the event.
+            args:    the event arguments containing the name and value of the changed property.
+        """
         if args.Name == "Length":
             self.box_length = args.Value
 
@@ -167,10 +166,8 @@ class UsingClrInteractor():
 
         self.draw_preview(self.coord_input.GetCurrentPoint().GetPoint())
 
-
-    def process_mouse_msg(self, mouse_msg, pnt, msg_info):
-        """
-        Process the mouse message event
+    def process_mouse_msg(self, mouse_msg: int, pnt: AllplanGeo.Point2D, msg_info: AllplanIFW.AddMsgInfo) -> bool:
+        """Handle mouse messages
 
         Args:
             mouse_msg:  the mouse message.
@@ -188,21 +185,18 @@ class UsingClrInteractor():
         if self.coord_input.IsMouseMove(mouse_msg):
             return True
 
-
-        #----------------- Create the line and continue with from point input
+        # ----------------- Create the line and continue with from point input
 
         AllplanBaseElements.CreateElements(self.coord_input.GetInputViewDocument(),
-                                            AllplanGeo.Matrix3D(),
-                                            self.model_ele_list, [], None)
+                                           AllplanGeo.Matrix3D(),
+                                           self.model_ele_list, [], None)
 
         self.coord_input.InitFirstPointInput(AllplanIFW.InputStringConvert("From point"))
 
         return True
 
-
-    def draw_preview(self, input_pnt):
-        """
-        Draw the preview
+    def draw_preview(self, input_pnt: AllplanGeo.Point3D):
+        """Draw the preview
 
         Args:
             input_pnt:  Input point
@@ -211,8 +205,9 @@ class UsingClrInteractor():
         box = AllplanGeo.Move(AllplanGeo.Polyhedron3D.CreateCuboid(self.box_length, self.box_width, self.box_height),
                               AllplanGeo.Vector3D(input_pnt))
 
-        self.model_ele_list = [AllplanBasisElements.ModelElement3D(self.com_prop, box)]
+        self.model_ele_list = ModelEleList()
+        self.model_ele_list.append_geometry_3d(box)
 
         AllplanBaseElements.DrawElementPreview(self.coord_input.GetInputViewDocument(),
-                                                AllplanGeo.Matrix3D(),
-                                                self.model_ele_list, True, None)
+                                               AllplanGeo.Matrix3D(),
+                                               self.model_ele_list, True, None)
